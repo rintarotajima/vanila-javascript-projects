049e2171aa104b46dcd9e7fd5a93b10c4724b27f
rintarotajima <rintarotajima@gmail.com>
2024-06-23 21:08:17 +0900

    [add] hex.jsにコメント

--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,5 +1,8 @@
+/* 16進数の16個の各値を配列 */
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+/* 16進数コードを表示するための要素を定数に代入 */
 const color = document.querySelector('.color');
+/* button要素を定数に代入 */
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', () => {
@@ -11,6 +14,7 @@ btn.addEventListener('click', () => {
     document.body.style.backgroundColor = hexColor;
 })
 
+/* ランダムで0~15の整数値を生成する関数 */
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length)
 }
