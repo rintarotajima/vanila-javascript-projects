e38a3c8ce13b197f74156e8ccd3f4213e08d8773
rintarotajima <rintarotajima@gmail.com>
2024-06-25 21:45:49 +0900

    [update] forEach内の処理にコメント

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -4,18 +4,36 @@ let count = 0;
 // HTML要素の取得
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
-// console.log(btns)`
 
 btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         const styles = e.currentTarget.classList;
+        /* decreaseボタンを押したときの処理*/
         if (styles.contains('decrease')) {
+            /* 現在のcountから値を-1 */
             count--;
-        } else if (styles.contains('increase')) {
+        }
+        /* increaseボタンを押したときの処理 */
+        else if (styles.contains('increase')) {
+            /* 現在のcountから値を+1; */
             count++;
-        } else if (styles.contains('reset')) {
+        } 
+        /* resetボタンを押したときの処理 */
+        else if (styles.contains('reset')) {
+            /* 現在のcountを0に戻す */
             count = 0;
         }
+
+        /* カウンターの色の更新 */
+        if (count > 0) {
+            value.style.color = 'green';
+        } else if (count < 0) {
+            value.style.color = 'red';
+        } else {
+            value.style.color = 'black';
+        }
+
+        /* カウンターの値を更新 */
         value.textContent = count;
     })
 })
